4fa3a70874633971e611d8fe54246e505d273e46
Julia Fokanova <oncologmma@mail.ru>
2026-04-02 20:42:16 +0300

    chore: rename project

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/RA_composition-yandex/',
+  base: '/yandex-portal/',
   server: {
     open: true,
   },
